5cba34757ffc69e3bbf647cf1f53832e066073de
Olivia Campbell <olivia@dxw.com>
2019-11-28 16:48:26 +0000

    Adding brew script along with README file
    The brew script installs Homebrew and basic apps for new starter

---
+++ README.md
@@ -0,0 +1,4 @@
+The purpose is to make setting setting up MacBooks less tedious by running
+a script which installs Homebrew and all the basic apps that a new starter needs
+and eventually a script which will set up the entire MacOs configuration itself
+so that you can go make a cup of tea and relax.
---
+++ brew-install-script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check that Homebrew is installed and install if not
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
+fi
+
+# Update any existing homebrew recipes
+brew update
+
+# Upgrade any already installed formulae
+brew upgrade
+
+# Install cask
+echo "Installing brew cask..."
+brew tap phinze/homebrew-cask
+
+#install basic apps
+
+brew cask install slack
+brew cask install 1password
+brew cask install google-chrome
+brew cask install firefox
