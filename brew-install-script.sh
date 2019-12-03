da62ba830cb8ba36c3fe76b6293ba10118bbac0b
Olivia Campbell <olivia@dxw.com>
2019-12-03 13:25:37 +0000

    Removed tmp log directory
    It was pointed out that the log directory means the user misses out on
    input prompts therefore, I've amended this.

--- brew-install-script.sh
+++ brew-install-script.sh
@@ -4,7 +4,7 @@
 if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > install.sh
 fi
 
 # Update any existing homebrew recipes
